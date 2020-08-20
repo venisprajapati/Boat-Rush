1af032c62328a7cb9d5359921f74efe252565e0e
Venis Prajapati <59964517+venisprajapati@users.noreply.github.com>
2020-08-20 15:04:46 +0530

    updated water in pygame boat-rush # 1.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
-
+from water import Water
 
 # Initialize Pygame
 pygame.init()
 
+# Screen set mode
 screen = pygame.display.set_mode((764, 664))  # ( width, height )
 
 # Title And Logo
@@ -11,9 +12,16 @@ pygame.display.set_caption("Boat Rush")  # display caption
 icon = pygame.image.load('ferryboat.png')
 pygame.display.set_icon(icon)
 
-# waves
-wave0 = pygame.image.load('wave0.png')
-wave1 = pygame.image.load('wave1.png')
+# defining water class
+w1 = Water()
+
+
+# Movement of water..
+def moving_water():
+    for i in range(w1.wave_len):
+        for j in range(w1.wave_wid):
+            screen.blit(w1.W_wave_img[i][j], (w1.W_wave_X[i][j], w1.W_wave_Y[i][j]))
+            w1.wave_move()
 
 
 # Game Loop.
@@ -21,14 +29,14 @@ running = True
 while running:  # running infinite while loop
 
     # Display screen (above everything)
-    # Anything that is persistent into your window continuously
-    screen.fill((100, 211, 219))  # (R,G,B) values (59, 179, 208) (0, 183, 229)
+    screen.fill((100, 211, 219))  # (R,G,B) values (59, 179, 208) , (0, 183, 229)
 
+    # break for close
     for event in pygame.event.get():  # see all the events happening in game window
         if event.type == pygame.QUIT:  # must write event type to check type of events and it is QUIT /=not quit,() :
             running = False  # break loop
-            
-    screen.blit(wave0, (50, 50))
-    screen.blit(wave1, (150, 150))
+
+    # Really water is moving
+    moving_water()
 
     pygame.display.update()  # update screen, if something new is added
---
+++ water.py
@@ -0,0 +1,51 @@
+import pygame
+import numpy as np
+
+
+class Water:
+
+    def __init__(self):
+
+        # Waves Formation
+        # self.W_wave_img_format = np.random.randint(0, 2, (14, 14))
+        self.wave_len = 15
+        self.wave_wid = 14
+        self.wave_diff_X = 64
+        self.wave_diff_Y = 50
+
+        self.wave_speed_X = 0.0055
+        self.wave_speed_Y = 0.0055
+
+        self.W_wave_img = []
+
+        for i in range(self.wave_len):
+            self.W_wave_img_X = []
+            for j in range(self.wave_wid):
+                self.W_wave_img_X.append(pygame.image.load('wave12.png'))
+            self.W_wave_img.append(self.W_wave_img_X)
+
+        self.wave_tmp_X = np.arange(-self.wave_diff_X, (self.wave_wid - 1) * self.wave_diff_X, self.wave_diff_X)
+
+        self.wave_tmp_Y = np.arange(-self.wave_diff_Y, (self.wave_len - 1) * self.wave_diff_Y, self.wave_diff_Y)
+
+        # self.W_wave_X = np.zeros((self.wave_len, self.wave_wid))
+        # self.W_wave_Y = np.zeros((self.wave_len, self.wave_wid))
+
+        self.W_wave_X = (np.vstack([self.wave_tmp_X] * self.wave_len)).astype('f')
+        self.W_wave_Y = ((np.vstack([self.wave_tmp_Y] * self.wave_wid)).astype('f')).T
+
+    def wave_move(self):
+
+        if self.W_wave_X[0][0] >= 0:
+            self.W_wave_X -= self.wave_diff_X
+        else:
+            self.W_wave_X += self.wave_speed_X
+
+        if self.W_wave_Y[0][0] >= 0:
+            self.W_wave_Y -= self.wave_diff_Y
+        else:
+            self.W_wave_Y += self.wave_speed_Y
+
+
+w = Water()
+w.wave_move()
