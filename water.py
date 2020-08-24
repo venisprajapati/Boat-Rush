2f320bab5bf11ec9a689994b603f6ac6824aef02
Venis Prajapati <59964517+venisprajapati@users.noreply.github.com>
2020-08-24 15:40:08 +0530

    updated wave bug in pygame boat-rush # 4.

--- water.py
+++ water.py
@@ -13,7 +13,7 @@ class Water:
         self.wave_diff_X = 58
         self.wave_diff_Y = 50
 
-        self.wave_speed_X = 0
+        self.wave_speed_X = 0.005
         self.wave_speed_Y = 0.01
 
         self.W_wave_img = []
@@ -32,16 +32,29 @@ class Water:
         # self.W_wave_Y = np.zeros((self.wave_len, self.wave_wid))
 
         self.W_wave_X = (np.vstack([self.wave_tmp_X] * self.wave_len)).astype('f')
+        self.W_wave_X_copy = np.copy(self.W_wave_X)
+        
         self.W_wave_Y = ((np.vstack([self.wave_tmp_Y] * self.wave_wid)).astype('f')).T
+        self.W_wave_Y_copy = np.copy(self.W_wave_Y)
 
     def wave_move(self):
 
         if self.W_wave_X[0][0] >= 0:
-            self.W_wave_X -= self.wave_diff_X
+            self.W_wave_X = np.copy(self.W_wave_X_copy)
+
+            # self.W_wave_X = np.floor(self.W_wave_X)
+            # print(type(self.W_wave_X[4][4]))
+            # self.W_wave_X -= self.wave_diff_X
         else:
             self.W_wave_X += self.wave_speed_X
 
         if self.W_wave_Y[0][0] >= 0:
-            self.W_wave_Y -= self.wave_diff_Y
+            self.W_wave_Y = np.copy(self.W_wave_Y_copy)
+
+            # print(self.W_wave_Y[4][4])
+            # self.W_wave_Y = np.floor(self.W_wave_Y)
+            # print(self.W_wave_Y[4][4])
+            # self.W_wave_Y -= self.wave_diff_Y
+            # print(self.W_wave_Y[4][4])
         else:
             self.W_wave_Y += self.wave_speed_Y
